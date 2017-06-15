cebc4ba2f986a523364bc6393dc62b5cc5cc1311
aliciasavelly <alicia.savelly@gmail.com>
2017-06-14 22:27:12 -0700

    clean up code

--- medianSortedArrays.js
+++ medianSortedArrays.js
@@ -1,32 +1,49 @@
 var findMedianSortedArrays = function(nums1, nums2) {
-    var midIdx;
-    var totalEls = nums1 + nums2;
-    var count = 0;
-    var result = 0;
+  let totalEls = nums1.length + nums2.length;
+  let midIdx;
+  let count = 0;
+  let result = 0;
+  let i = 0;
+  let j = 0;
+  let current = 0;
 
-    if (totalEls % 2 === 0) {
-        midIdx = [totalEls / 2 - 1, totalEls / 2 ];
-    } else {
-        midIdx = [Math.floor(totalEls / 2)];
+  if (totalEls % 2 === 0) {
+    midIdx = [totalEls / 2 - 1, totalEls / 2 ];
+  } else {
+    midIdx = [Math.floor(totalEls / 2)];
+  }
+
+  while (count <= midIdx[midIdx.length - 1]) {
+    if (nums1[i] === undefined || nums2[j] < nums1[i]) {
+      current = nums2[j];
+      j++;
+    } else if (nums2[j] === undefined) {
+      current = nums1[i];
+      i++
+    } else if (nums1[i] <= nums2[j]) {
+      current = nums1[i];
+      i++;
+    } else if (nums2[j] < nums1[i]) {
+      current = nums2[j];
+      j++;
     }
 
-    let i = 0;
-    let j = 0;
-    let current = 0;
-    while (count <= midIdx[midIdx.length - 1]) {
-        if (nums1[i] === undefined) {
-            current = nums2[j];
-            j++;
-        } else if (nums2[j] === undefined) {
-            current = nums1[i];
-            i++
-        } else if (nums1[i] <= nums2[j]) {
-            current = nums1[i];
-            i++;
-        } else if (nums2[j] < nums1[j]) {
-            current = nums2[j];
-            j++;
-        }
+    if (count == midIdx[0]) {
+      if (midIdx.length == 1) {
+        return current;
+      } else {
+        result += current;
+      }
+    } else if (midIdx.length == 2 && count == midIdx[midIdx.length - 1]) {
+      result += current;
+      return result / 2;
+    }
 
-        if (count == midIdx[0]) {
-            if (midIdx.length == 1) {
+    count++;
+  }
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]) === 2);
+console.log(findMedianSortedArrays([1, 2], [3, 4]) === 2.5);
+console.log(findMedianSortedArrays([1, 9], [1, 2, 7, 10]) === 4.5);
+console.log(findMedianSortedArrays([1, 4], [2, 3, 6, 7, 9]) === 4);
