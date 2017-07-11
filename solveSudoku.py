40edac851634520b4bbca09205444ed799ce8c5b
aliciasavelly <alicia.savelly@gmail.com>
2017-07-10 21:28:23 -0700

    write find empty location method

--- solveSudoku.py
+++ solveSudoku.py
@@ -4,6 +4,14 @@ def print_grid(arr):
             print(str(arr[i][j]) + " "),
         print ('')
 
+def find_empty_location(arr, l):
+    for row in range(9):
+        for col in range(9):
+            if arr[row][col] == 0:
+                l[0] = row
+                l[1] = col
+                return True
+
 print print_grid([[3,0,6,5,0,8,4,0,0],
                   [5,2,0,0,0,0,0,0,0],
                   [0,8,7,0,0,0,0,3,1],
