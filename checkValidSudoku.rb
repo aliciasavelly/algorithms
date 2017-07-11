224bab1304a6e913d82c83b94d31e694013e8b11
aliciasavelly <alicia.savelly@gmail.com>
2017-07-10 22:12:44 -0700

    add check squares for sudoku

--- checkValidSudoku.rb
+++ checkValidSudoku.rb
@@ -38,5 +38,29 @@ def check_cols(board)
 end
 
 def check_squares(board)
+  pos = [0, 0]
+  while pos[0] < 9 && pos[1] < 9
+    total = 0
+    (0..2).each do |row|
+      (0..2).each do |col|
+        val = board[row + pos[0]][col + pos[1]]
+        return false if val == 0
+        return false if val > 9 || val < 0
+        total += val
+      end
+    end
 
+    return false if total != 45
+
+    if pos[0] == 6 and pos[1] == 6
+      pos = [9, 9]
+    elsif pos[0] == 6
+      pos[0] = 0
+      pos[1] += 3
+    else
+      pos[0] += 3
+    end
+  end
+
+  true
 end
