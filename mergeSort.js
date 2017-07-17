b6545552add94454ebfb6671dc46f26f319a58a6
aliciasavelly <alicia.savelly@gmail.com>
2017-07-17 16:34:54 -0700

    finish js merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -6,16 +6,30 @@ function mergeSort(arr, callback) {
   let mid = Math.floor(arr.length / 2);
   let left = arr.slice(0, mid);
   let right = arr.slice(mid, arr.length);
-  let leftSorted = mergeSort(left);
-  let rightSorted = mergeSort(right);
+  let leftSorted = mergeSort(left, callback);
+  let rightSorted = mergeSort(right, callback);
 
   return merge(leftSorted, rightSorted, callback);
 }
 
 function merge(arr1, arr2, callback) {
-  callback ||= function(num1, num2) {
+  callback = callback || function(num1, num2) {
     if (num1 < num2) {
       return -1;
     }
   }
+
+  let result = [];
+
+  while (arr1.length > 0 && arr2.length > 0) {
+    if (callback(arr1, arr2) === -1) {
+      result.push(arr1.shift());
+    } else {
+      result.push(arr2.shift());
+    }
+  }
+
+  return result.concat(arr1).concat(arr2);
 }
+
+console.log(mergeSort([3, 2, 1, 2, 3, 6, 9, 1, 2, 3]));
