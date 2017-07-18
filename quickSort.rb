051615f43d1eb2d0a5443bee4ccf4afcfb54fe3b
aliciasavelly <alicia.savelly@gmail.com>
2017-07-17 18:09:31 -0700

    write class method form for rb quicksort

--- quickSort.rb
+++ quickSort.rb
@@ -10,3 +10,19 @@ def quickSort(arr, &comparator)
 end
 
 p quickSort([3, 2, 1, 2, 3, 6, 9, 1, 2, 3])
+
+class Array
+  def my_quick_sort(&prc)
+    return self if self.length < 2
+    prc ||= Proc.new { |x, y| x <=> y }
+
+    pivot = self.first
+    left = self[1..-1].select { |el| prc.call(el, pivot) == -1 }
+    right = self[1..-1].select { |el| prc.call(el, pivot) != -1 }
+
+    left.my_quick_sort(&prc) + [pivot] + right.my_quick_sort(&prc)
+  end
+end
+
+a = [3, 2, 1, 2, 3, 6, 9, 1, 2, 3]
+p a.my_quick_sort()
