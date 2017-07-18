3343ac707de0bf1e39fecf976fbadc3fc51beba4
aliciasavelly <alicia.savelly@gmail.com>
2017-07-18 11:22:01 -0700

    work on dictionary flatten for python

--- flattenDict.py
+++ flattenDict.py
@@ -1,11 +1,60 @@
-def flatten(dictionary):
+def flatten(dictionary = -1):
+    if dictionary == -1 or dictionary == {}:
+        return -1
+
     output = {}
     flattenHelper("", dictionary, output)
     return output
 
 def flattenHelper(prefix, input, output):
     if (type(input) != dict):
-        output[prefix] = input
+        output[prefix[1:len(prefix)]] = input
         return
 
-    
+    for key in input:
+        newPrefix = prefix + "." + str(key)
+        flattenHelper(newPrefix, input[key], output)
+
+dictionary = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": {
+        "f": 5,
+        "g": 6,
+        "h": {
+            "i": 7,
+            "j": {
+                "k": 8
+            }
+        }
+    }
+}
+
+dictionary2 = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": {
+        "f": 5,
+        "g": 6,
+        "h": {
+            "i": 7,
+            "j": {
+                "k": 8
+            }
+        }
+    },
+    9: 10
+}
+
+print flatten(dictionary)
+print flatten(dictionary) == {'a': 1, 'e.h.i': 7, 'c': 3, 'b': 2, 'd': 4, 'e.f': 5, 'e.g': 6, 'e.h.j.k': 8}
+
+print flatten(dictionary2)
+print flatten(dictionary2) == {'a': 1, 'e.h.i': 7, 'c': 3, 'b': 2, 'd': 4, 'e.f': 5, 'e.g': 6, '9': 10, 'e.h.j.k': 8}
+
+print flatten()
+print flatten({})
