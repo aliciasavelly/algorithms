6d459c3af8322100aac8fc83a38df38dcca85efc
aliciasavelly <alicia.savelly@gmail.com>
2017-07-17 18:28:41 -0700

    finish class method rb version of merge sort

--- mergeSort.rb
+++ mergeSort.rb
@@ -1,3 +1,5 @@
+# require 'byebug'
+
 def mergeSort(arr, &callback)
   return arr if arr.length < 2
   callback ||= Proc.new { |x, y| x <=> y }
@@ -15,10 +17,10 @@ def merge(arr1, arr2, &callback)
   result = []
 
   while arr1.length > 0 && arr2.length > 0
-    if (callback.call(arr1[0], arr2[0]) < 0)
-      result << arr1.shift()
+    if callback.call(arr1[0], arr2[0]) < 0
+      result << arr1.shift
     else
-      result << arr2.shift()
+      result << arr2.shift
     end
   end
 
@@ -32,16 +34,28 @@ class Array
     return self if self.length < 2
     prc ||= Proc.new { |x, y| x <=> y }
 
-    mid = self.length
+    mid = self.length / 2
     left = self.slice(0, mid)
     right = self.slice(mid, self.length)
     leftSorted = left.merge_sort(&prc)
     rightSorted = right.merge_sort(&prc)
 
-    Array.merge(left, right, &prc)
+    Array.merge(leftSorted, rightSorted, &prc)
   end
 
   def self.merge(arr1, arr2, &prc)
+    result = []
 
+    while arr1.length > 0 && arr2.length > 0
+      if prc.call(arr1[0], arr2[0]) < 0
+        result << arr1.shift
+      else
+        result << arr2.shift
+      end
+    end
+
+    result + arr1 + arr2
   end
 end
+
+p [7, 2, 1, 4, 5, 6, 2, 7, 19, 9, 3].merge_sort()
