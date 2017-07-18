6f59b7e668909f73754c6b2fc30a7f73ae1f2f1d
aliciasavelly <alicia.savelly@gmail.com>
2017-07-17 17:31:52 -0700

    write merge sort in ruby

---
+++ mergeSort.rb
@@ -0,0 +1,28 @@
+def mergeSort(arr, &callback)
+  return arr if arr.length < 2
+  callback ||= Proc.new { |x, y| x <=> y }
+
+  mid = arr.length / 2
+  left = arr.slice(0, mid)
+  right = arr.slice(mid, arr.length)
+  leftSorted = mergeSort(left, &callback)
+  rightSorted = mergeSort(right, &callback)
+
+  merge(leftSorted, rightSorted, &callback)
+end
+
+def merge(arr1, arr2, &callback)
+  result = []
+
+  while arr1.length > 0 && arr2.length > 0
+    if (callback.call(arr1[0], arr2[0]) < 0)
+      result << arr1.shift()
+    else
+      result << arr2.shift()
+    end
+  end
+
+  result + arr1 + arr2
+end
+
+p mergeSort([7, 2, 1, 4, 5, 6, 2, 7, 19, 9, 3])
