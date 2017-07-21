e69c0f00de924ec8b12c8a686d98d0d52e9fbed3
aliciasavelly <alicia.savelly@gmail.com>
2017-07-21 15:36:17 -0700

    add who eats who

---
+++ whoEatsWho.js
@@ -0,0 +1,44 @@
+var whoEatsWho = function(zoo) {
+  let animalFood = {
+    "antelope": new Set(["grass"]),
+    "big-fish": new Set(["little-fish"]),
+    "bug": new Set(["leaves"]),
+    "bear": new Set(["big-fish", "bug", "chicken", "cow", "leaves", "sheep"]),
+    "chicken": new Set(["bug"]),
+    "cow": new Set(["grass"]),
+    "fox": new Set(["chicken", "sheep"]),
+    "giraffe": new Set(["leaves"]),
+    "lion": new Set(["antelope", "cow"]),
+    "panda": new Set(["leaves"]),
+    "sheep": new Set(["grass"])
+  };
+
+  let dupZoo = zoo.split(",");
+  let result = [];
+
+  for (let i = 0; i < dupZoo.length; i++) {
+//     console.log(dupZoo);
+//     console.log(i);
+    if (animalFood[dupZoo[i]]) {
+      if (dupZoo[i - 1] && animalFood[dupZoo[i]].has(dupZoo[i - 1])) {
+        result.push(dupZoo[i] + " eats " + dupZoo[i - 1]);
+//         console.log(result);
+        dupZoo.splice(i - 1, 1);
+        i = -1;
+      } else if (dupZoo[i + 1] && animalFood[dupZoo[i]].has(dupZoo[i + 1])) {
+        result.push(dupZoo[i] + " eats " + dupZoo[i + 1]);
+//         console.log(result);
+        dupZoo.splice(i + 1, 1);
+        i = -1;
+      }
+    }
+  }
+
+//   console.log(result);
+//   console.log(dupZoo.join());
+  result.push(dupZoo.join());
+  result.unshift(zoo);
+  console.log(result);
+//   console.log(result.unshift(zoo));
+  return result;
+}
