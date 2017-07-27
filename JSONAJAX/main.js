c131c09f8c9477fed39598339c95d377948d2c22
aliciasavelly <alicia.savelly@gmail.com>
2017-07-26 22:13:05 -0700

    finish up, and clean up code

--- JSONAJAX/main.js
+++ JSONAJAX/main.js
@@ -4,12 +4,26 @@ let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
   let ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+  ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
+
   ourRequest.onload = function() {
-    let ourData = JSON.parse(ourRequest.responseText);
-    renderHTML(ourData);
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      let ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+
+    } else {
+      console.log("We connected to the server but it returned an error.");
+      alert("We connected to the server but it returned an error.");
+    }
   };
+
+  ourRequest.onerror = function() {
+    console.log("Connection error.");
+    alert("Connection error.");
+  };
+
   ourRequest.send();
+
   pageCounter++;
   if (pageCounter > 3) {
     btn.classList.add("hide-me");
@@ -20,8 +34,28 @@ function renderHTML(data) {
   let htmlString = "";
 
   for (let i = 0; i < data.length; i++) {
-    htmlString += "<p>" + data[i].name + " the " + data[i].species + ".</p>";
+
+    htmlString += `<p>${data[i].name} the ${data[i].species} likes to eat `;
+    for (let j = 0; j < data[i].foods.likes.length; j++) {
+      if (j == 0) {
+        htmlString += data[i].foods.likes[j];
+      } else {
+        htmlString += ` and ${data[i].foods.likes[j]}`;
+      }
+    }
+
+    htmlString += ' and dislikes eating ';
+    for (let j = 0; j < data[i].foods.dislikes.length; j++) {
+      if (j == 0) {
+        htmlString += data[i].foods.dislikes[j];
+      } else {
+        htmlString += ` and ${data[i].foods.dislikes[j]}`;
+      }
+    }
+    
+    htmlString += `.</p>`;
   }
+
 
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
 }
