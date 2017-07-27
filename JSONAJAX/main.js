9968717e9fce2885ce58afcf67f672251fd765dc
aliciasavelly <alicia.savelly@gmail.com>
2017-07-26 21:40:12 -0700

    make button click lead to ajax request to different pages, and button disappear when needed

--- JSONAJAX/main.js
+++ JSONAJAX/main.js
@@ -1,10 +1,27 @@
+let pageCounter = 1;
+let animalContainer = document.getElementById("animal-info");
 let btn = document.getElementById("btn");
+
 btn.addEventListener("click", function() {
   let ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
   ourRequest.onload = function() {
     let ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
+    renderHTML(ourData);
   };
   ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add("hide-me");
+  }
 });
+
+function renderHTML(data) {
+  let htmlString = "";
+
+  for (let i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name + " the " + data[i].species + ".</p>";
+  }
+
+  animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
