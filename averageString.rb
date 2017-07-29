979db466fa901d5eeeff3cea597fc8acf9279c25
aliciasavelly <alicia.savelly@gmail.com>
2017-07-29 16:04:07 -0700

    add average string

---
+++ averageString.rb
@@ -0,0 +1,26 @@
+def average_string(s)
+  return "n/a" unless s.length > 0
+
+  total = 0
+  s_split = s.split(" ")
+  numbers = {
+    "zero" => 0,
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9
+  }
+
+  s_split.each do |num|
+    return "n/a" unless numbers[num]
+
+    total += numbers[num]
+  end
+
+  numbers.key(total / s_split.length)
+end
