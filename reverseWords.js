78c9b320a963400c9f7a1198a80c7c3680b09842
aliciasavelly <alicia.savelly@gmail.com>
2017-08-01 17:34:02 -0700

    add an in-place version for reverse words

--- reverseWords.js
+++ reverseWords.js
@@ -1,14 +1,28 @@
-function reverseWords(message) {
-  let result = [];
-  let messageArr = message.split(" ");
+// basic method
+// function reverseWords(message) {
+//   let result = [];
+//   let messageArr = message.split(" ");
+//
+//   for (let i = messageArr.length - 1; i >= 0; i--) {
+//     result.push(messageArr[i]);
+//   }
+//
+//   return result.join(" ");
+// }
 
-  for (let i = messageArr.length - 1; i >= 0; i--) {
-    result.push(messageArr[i]);
+// in-place method
+function reverseWords(message) {
+  let messageArr = message.split(" ");
+  let len = messageArr.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    let temp = messageArr[len - 1 - i];
+    messageArr[len - 1 - i] = messageArr[i];
+    messageArr[i] = temp;
   }
 
-  return result.join(" ");
+  return messageArr.join(" ");
 }
 
 let message = 'find you will pain only go you recordings security the into if';
-
 console.log(reverseWords(message) == 'if into the security recordings you go only pain will you find');
