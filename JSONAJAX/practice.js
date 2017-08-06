d9e5cc71165070f8635e969861631be9f3f5db51
aliciasavelly <alicia.savelly@gmail.com>
2017-08-05 20:16:48 -0700

    edit practice.js

--- JSONAJAX/practice.js
+++ JSONAJAX/practice.js
@@ -4,15 +4,17 @@ let htmlPage;
 
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    debugger;
     htmlPage = JSON.parse(this.responseText);
     logPage(htmlPage);
   }
 }
 
-xmlhttp.open('GET', 'json.txt');
+xmlhttp.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 xmlhttp.send('');
 
 function logPage(arr) {
   console.log(arr);
+  htmlPage.forEach( el => {
+    console.log(el);
+  });
 }
