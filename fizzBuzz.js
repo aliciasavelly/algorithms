eebcc1b5f356f2573bc0408ea5e56515e5941cb0
aliciasavelly <alicia.savelly@gmail.com>
2017-08-07 20:06:57 -0700

    add another version of fizzbuzz for js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,14 +1,23 @@
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 15 === 0) {
+//       console.log("fizzbuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
 function fizzBuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    if (i % 15 === 0) {
-      console.log("fizzbuzz");
-    } else if (i % 3 === 0) {
-      console.log("fizz");
-    } else if (i % 5 === 0) {
-      console.log("buzz");
-    } else {
-      console.log(i);
-    }
+  let msg;
+  for (let i = 1; i <= n; i++, msg = "") {
+    if (!(i % 3)) msg += "fizz";
+    if (!(i % 5)) msg += "buzz";
+    console.log(msg || i);
   }
 }
 
