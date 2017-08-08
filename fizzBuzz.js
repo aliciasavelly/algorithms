f40cbda265a5a7f667960c135053686a236bc452
aliciasavelly <alicia.savelly@gmail.com>
2017-08-07 20:02:04 -0700

    improve fizzbuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,9 @@
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0) {
-      if (i % 5 === 0) {
-        console.log("fizzbuzz");
-      } else {
-        console.log("fizz");
-      }
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
     } else if (i % 5 === 0) {
       console.log("buzz");
     } else {
--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,9 @@
 def fizz_buzz(n)
   1.upto(n).each do |num|
-    if num % 3 == 0
-      if num % 5 == 0
-        p "fizzbuzz"
-      else
-        p "fizz"
-      end
+    if num % 15 == 0
+      p "fizzbuzz"
+    elsif num % 3 == 0
+      p "fizz"
     elsif num % 5 == 0
       p "buzz"
     else
