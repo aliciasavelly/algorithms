41f56da75715e4b8b451716fce57f2558437d302
aliciasavelly <alicia.savelly@gmail.com>
2017-08-14 14:50:25 -0700

    work with regex

---
+++ Coursera/regex.js
@@ -0,0 +1,34 @@
+l+
+l
+ll
+lll
+llll
+0l
+sdf
+sdlv
+9
+
+l*
+
+llll
+lll
+l
+s
+
+lll*
+
+lls
+ll
+l
+
+[lll]*
+[abc]*
+
+
+// first character in the string in one group
+// rest of the characters in another group
+let str = 'hello my name is sresht';
+
+let reg = /(.*?)(sresht)(.*)/;
+
+let newstr = group1 /*everything up to sresht*/ + /alicia/ + /*everything after sresht*/;
