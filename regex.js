a67d2d6d5f28f34b5f44760c2687d4f42edd9b05
aliciasavelly <alicia.savelly@gmail.com>
2017-08-20 16:45:56 -0700

    work on regex

--- regex.js
+++ regex.js
@@ -1,4 +1,4 @@
-let str = "foot fool buffoon".replace(/foo/g, "bar");
+let str1 = "foot fool buffoon".replace(/foo/g, "bar");
 console.log(str);
 console.log(str === "bart barl bufbarn");
 
@@ -9,3 +9,12 @@ console.log(match2);
 // returns all in string that matches it
 
 let str3 = "(248)842-6297";
+let match3 = str3.replace(/[()-]/g, "");
+console.log(match3);
+console.log(match3 === "2488426297");
+
+let str4 = "1,2, 3,  4,     5";
+let match4 = str4.split(/\s*,\s*/);
+console.log(match4);
+// ['1', '2', '3', '4', '5']
+console.log(str4);
