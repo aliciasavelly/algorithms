00b5f3e3b227cd67a5a1ccab5aab72997b952e35
aliciasavelly <alicia.savelly@gmail.com>
2017-08-21 12:29:43 -0700

    work on password not regex

--- pass.js
+++ pass.js
@@ -8,8 +8,48 @@
 
 // return bool of whether or not string is valid password
 
+// function validPassword(password) {
+//   return /^(?=.{6,50}$)(?=.*\W)(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$/.test(password);
+// }
+
 function validPassword(password) {
-  return /^(?=.{6,50}$)(?=.*\W)(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$/.test(password);
+  let len = password.length;
+  if (len < 6 || len > 50) {
+    return false;
+  }
+
+  let checks = { "special": false, "num": false, "lower": false, "upper": false };
+  let checkCount = 0;
+
+  for (let i = 0; i <= password.length - checkCount; i++) {
+    if (checkCount === 4) {
+      return true;
+    }
+
+    let passCode = password.charCodeAt(i);
+    if (!checks["special"] && ((passCode >= 33 && passCode <= 47) || (passCode >= 58 && passCode <= 64))) {
+      checks["special"] = true;
+      checkCount++;
+      continue;
+    }
+    if (!checks["num"] && (passCode >= 30 || passCode <= 39)) {
+      checks["num"] = true;
+      checkCount++;
+      continue;
+    }
+    if (!checks["lower"] && (passCode >= 97 && passCode <= 122)) {
+      checks["lower"] = true;
+      checkCount++;
+      continue;
+    }
+    if (!checks["upper"] && (passCode >= 65 && passCode <= 90)) {
+      checks["upper"] = true;
+      checkCount++;
+      continue;
+    }
+  }
+
+  return checkCount === 4;
 }
 
 function logValidity(passwords) {
@@ -19,6 +59,6 @@ function logValidity(passwords) {
 }
 
 let passwords = ["$lkJl800alskfjlskfjlskfjlskfjlskfjlskfjlskfjlskfjlskfj",
-                 "abc", "alicia", "%1lA", "#2aAlk",
+                 "abc", "alicia", "%1lA", "#2aAlk", "all$I2",
                  "$lkJl800alskfjlskfjlskfjlskfjlskfjljlskfj"];
 logValidity(passwords);
