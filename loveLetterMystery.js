19be5ca2355e8acfc27030d51e57d38de10faef7
aliciasavelly <alicia.savelly@gmail.com>
2017-08-23 11:56:15 -0700

    add love letter mystery

---
+++ loveLetterMystery.js
@@ -0,0 +1,34 @@
+function theLoveLetterMystery(s){
+  let half1;
+  let half2;
+  let len = s.length;
+  let total = 0;
+
+  if (len % 2 === 0) {
+    half1 = s.slice(0, len / 2);
+    half2 = reverse(s.slice(len / 2, len));
+  } else {
+    half1 = s.slice(0, Math.floor(len / 2));
+    half2 = reverse(s.slice(Math.ceil(len / 2), len));
+  }
+
+  if (half1 && half1 === half2) {
+    return 0;
+  }
+
+  for (let i = 0; i < half1.length; i++) {
+    total += Math.abs(half1.charCodeAt(i) - half2.charCodeAt(i));
+  }
+
+  return total;
+}
+
+function reverse(string) {
+  let reversed = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+
+  return reversed;
+}
