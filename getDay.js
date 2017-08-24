fe42c36b874203bcbe66cc9a80d1c5eea4ec2bf5
aliciasavelly <alicia.savelly@gmail.com>
2017-08-23 23:08:41 -0700

    add get day

---
+++ getDay.js
@@ -0,0 +1,29 @@
+function getDayName(dateString) {
+  let dayName = new Date(dateString).getDay();
+
+  switch (dayName) {
+    case 0:
+      dayName = "Sunday";
+      break;
+    case 1:
+      dayName = "Monday";
+      break;
+    case 2:
+      dayName = "Tuesday";
+      break;
+    case 3:
+      dayName = "Wednesday";
+      break;
+    case 4:
+      dayName = "Thursday";
+      break;
+    case 5:
+      dayName = "Friday";
+      break;
+    case 6:
+      dayName = "Saturday";
+      break;
+  }
+
+  return dayName;
+}
