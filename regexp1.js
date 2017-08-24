51b3cee6df9f8e782f3c8b67dc230c809f4d4b34
aliciasavelly <alicia.savelly@gmail.com>
2017-08-23 23:20:50 -0700

    work with regex

---
+++ regexp1.js
@@ -0,0 +1,8 @@
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+   */
+  let re = /^([aeiou]).*\1$/
+  return re;
+}
