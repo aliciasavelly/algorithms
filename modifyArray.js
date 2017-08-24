4f33c6c444526e1682e05dcefb2d0c7749cdb4ea
aliciasavelly <alicia.savelly@gmail.com>
2017-08-23 23:00:21 -0700

    change modify array

---
+++ modifyArray.js
@@ -0,0 +1,13 @@
+function modifyArray(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      nums[i] *= 2;
+    } else {
+      nums[i] *= 3;
+    }
+  }
+
+  return nums;
+}
+
+console.log(modifyArray([1, 2, 3, 4, 5]));
