58b46166b042c0850787b202abe24281b5870411
aliciasavelly <alicia.savelly@gmail.com>
2017-10-16 20:14:29 -0700

    improve alternating chars code and add test cases

--- alternatingChars.js
+++ alternatingChars.js
@@ -1,14 +1,29 @@
-function alternatingCharacters(s){
+// This method returns the number of repeated characters in a string
+
+function alternatingCharacters(sentence){
   let total = 0;
   let current;
 
-  for (let i = 0; i < s.length; i++) {
-    if (current != s[i]) {
-      current = s[i];
+  for (let i = 0; i < sentence.length; i++) {
+    if (current != sentence[i]) {
+      current = sentence[i];
     } else {
-      total += 1;
+      total++;
+      while (current == sentence[i + 1]) {
+        i++;
+      }
     }
   }
 
   return total;
 }
+
+console.log("Alternating Characters");
+console.log(alternatingCharacters("allicia hi") == 1);
+console.log(alternatingCharacters("coding is cool") == 1);
+console.log(alternatingCharacters("hheelllloo") == 4);
+console.log(alternatingCharacters("cooooodiiing") == 2);
+console.log(alternatingCharacters("allicia hi"));
+console.log(alternatingCharacters("coding is cool"));
+console.log(alternatingCharacters("hheelllloo"));
+console.log(alternatingCharacters("cooooodiiing"));
