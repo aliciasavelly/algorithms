66ea578d5604b960af7acdc3610cfbee1d1dad72
aliciasavelly <alicia.savelly@gmail.com>
2017-10-16 20:18:53 -0700

    add test case and description for arr product

--- arrProduct.js
+++ arrProduct.js
@@ -1,3 +1,8 @@
+// arrProduct returns an array of the same length as the input (arr)
+// The result array will give us the total product of all the numbers
+// included in the input array except for the number at the same position
+// [1, 2, 3] => [2 * 3, 1 * 3, 1 * 2] => [6, 3, 2]
+
 function arrProduct(arr) {
   let products = [];
   let total = totalProduct(arr);
@@ -21,3 +26,5 @@ function totalProduct(arr) {
 
 console.log(arrProduct([1, 3, 2, 6, 7, 4]));
 // [1008, 336, 504, 168, 144, 252]
+console.log(arrProduct([1, 2, 3]));
+// [6, 3, 2]
